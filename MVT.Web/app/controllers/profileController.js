2ca52d8793abdb473f39ed445a4d228c870c6396
nomiz_007 <Nouman_2094@yahoo.com>
2016-01-05 18:28:32 +0500

    Modify User Module design and add slider to layout and logo of application and also slider images

--- MVT.Web/app/controllers/profileController.js
+++ MVT.Web/app/controllers/profileController.js
@@ -2,23 +2,27 @@
 app.controller('profileController', ['$scope', 'profileService', function ($scope, profileService) {
     $scope.profile = {};
     $scope.isEdit = false;
+    $scope.ishide = true;
     $scope.message = "";
     $scope.savedSuccessfully = false;
 
 
     $scope.edit = function () {
+        $scope.ishide = false;
         $scope.isEdit = true;
+      
     };
 
     $scope.cancel = function () {
         $scope.isEdit = false;
+        $scope.ishide = true;
+        $scope.message = '';
     };
 
     $scope.save = function (isValid) {
         if (isValid.$valid){
             profileService.updateProfile($scope.profile).then(function (response) {
                 $scope.message = 'Profile Updated..';
-                $scope.isEdit = false;
                 $scope.savedSuccessfully = true;
             }, function (error) {
                 $scope.message = 'Profile Not Updated!!';
